d2da436e224cccecd6e4eedf58d6bfa1d30f0fff
hammy275 <hammy275@gmail.com>
2025-01-15 15:47:14 -0500

    Just Expose Hands

--- common/src/main/java/org/vivecraft/api/client/VRRenderingAPI.java
+++ common/src/main/java/org/vivecraft/api/client/VRRenderingAPI.java
@@ -4,7 +4,6 @@ import com.google.common.annotations.Beta;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.phys.Vec3;
-import org.vivecraft.api.client.data.RenderPosOption;
 import org.vivecraft.client.api_impl.VRRenderingAPIImpl;
 import org.vivecraft.client_vr.render.RenderPass;
 
@@ -34,21 +33,20 @@ public interface VRRenderingAPI {
     boolean isFirstRenderPass();
 
     /**
-     * Gets the position that the provided {@link RenderPosOption} renders at. Unlike
-     * {@link org.vivecraft.api.data.VRPose#getHand(InteractionHand)} for {@link VRClientAPI#getWorldRenderPose()}, this
-     * returns a reasonable, default value for seated mode and can also be used to retrieve the rendering position
-     * for the third person camera.
-     * @param option The rendering position to be returned.
+     * Gets the position that the provided {@link InteractionHand} renders at. Unlike
+     * {@link org.vivecraft.api.data.VRPose#getHand(InteractionHand)} from {@link VRClientAPI#getWorldRenderPose()},
+     * this returns a reasonable, default value for seated mode.
+     * @param hand The hand to get the rendering position of.
      * @return The rendering position for the provided hand.
      */
     @Beta
-    Vec3 getRenderPos(RenderPosOption option);
+    Vec3 getHandRenderPos(InteractionHand hand);
 
     /**
-     * Sets the provided {@link PoseStack} to render at the position provided.
-     * @param option The rendering position to set the PoseStack to.
+     * Sets the provided {@link PoseStack} to render at the position of the provided {@link InteractionHand}.
+     * @param hand The hand to set the PoseStack to.
      * @param stack The PoseStack to be set.
      */
     @Beta
-    void setupRendering(RenderPosOption option, PoseStack stack);
+    void setupRenderingAtHand(InteractionHand hand, PoseStack stack);
 }
--- common/src/main/java/org/vivecraft/client/api_impl/VRRenderingAPIImpl.java
+++ common/src/main/java/org/vivecraft/client/api_impl/VRRenderingAPIImpl.java
@@ -1,9 +1,9 @@
 package org.vivecraft.client.api_impl;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.phys.Vec3;
 import org.vivecraft.api.client.VRRenderingAPI;
-import org.vivecraft.api.client.data.RenderPosOption;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.render.RenderPass;
 import org.vivecraft.client_vr.render.helpers.RenderHelper;
@@ -32,12 +32,12 @@ public class VRRenderingAPIImpl implements VRRenderingAPI {
     }
 
     @Override
-    public Vec3 getRenderPos(RenderPosOption option) {
-        return RenderHelper.getControllerRenderPos(option.ordinal());
+    public Vec3 getHandRenderPos(InteractionHand hand) {
+        return RenderHelper.getControllerRenderPos(hand.ordinal());
     }
 
     @Override
-    public void setupRendering(RenderPosOption option, PoseStack stack) {
-        RenderHelper.setupRenderingAtController(option.ordinal(), stack);
+    public void setupRenderingAtHand(InteractionHand hand, PoseStack stack) {
+        RenderHelper.setupRenderingAtController(hand.ordinal(), stack);
     }
 }
